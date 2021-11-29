ba59d68c5fe066eb0e34074d69ec8c48194a6f5c
CheezeLatte <lee50505223@gmail.com>
2021-11-29 21:35:30 +0900

    Fix: Add Comments Res Fixed:

--- prepare/back/src/routes/user/controller.js
+++ prepare/back/src/routes/user/controller.js
@@ -75,7 +75,7 @@ exports.addComments = asyncHandler(async(req, res)=> {
     .populate({path:"writer", select:'nickname' })
   await post.updateOne({$push:{comments:documents._id}})
 
-  res.json({success:true, status: 200, message: "Comment Added", data:{postId:id, comment:data.comment, writer:data.writer}});
+  res.json({success:true, status: 200, message: "Comment Added", data:{postId:id, comment:data.comment, writer:data.writer, commentsId:documents._id}});
 })
 
 exports.removeComments = asyncHandler(async(req,res) => {
